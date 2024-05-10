f1eabc97c0f74255a82828ef9aa5d9e60c02a7f9
GitArtyon <45424953+GitArtyon@users.noreply.github.com>
2024-05-10 14:59:14 +0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,59 +1,72 @@
 #!/bin/bash
+# https://github.com/GitArtyon/Sharings/blob/main/script.sh
+# UTILS
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+# ENV
+NG_PROMTAIL_BINARY_ARCHIVE="promtail-linux-amd64.zip"
+NG_PROMTAIL_BINARY="promtail-linux-amd64"
+NG_PROMTAIL_RELEASE="v3.0.0"
+NG_PROMTAIL_CONFIG_NAME="promtail-local-config.yaml"
+NG_PROMTAIL_DIR="$HOME/.promtail"
+NG_PROMTAIL_LOKI_SERVER_ADDRESS="195.201.98.42:3100"
+
 # Checking Promtail installation
-if apt-cache policy promtail | grep none
-then
-echo "Promtail is not installed"
-cd $HOME
-echo "Zip app will be installed to help Promtail installation"
-sudo apt-get install unzip -y
-sudo wget https://github.com/grafana/loki/releases/download/v3.0.0/promtail-linux-amd64.zip
-sudo unzip ./promtail-linux-amd64.zip
-sudo wget https://raw.githubusercontent.com/grafana/loki/main/clients/cmd/promtail/promtail-local-config.yaml
-sudo sed -i 's/localhost:3100/195.201.98.42:3100/g' promtail-local-config.yaml
-sudo sed -i 's|*log|caddy/*log|g' promtail-local-config.yaml
-echo "Promtail installed and configured"
+if exists $NG_PROMTAIL_BINARY; then
+    echo "Promtail installed in: $(which $NG_PROMTAIL_BINARY)"
+    cd $HOME
 else
-echo "Promtail installed in:"
-which promtail
-cd $HOME
+    echo "Promtail is not installed"
+    mkdir $NG_PROMTAIL_DIR
+    cd $NG_PROMTAIL_DIR
+    echo "Zip app will be installed to help Promtail installation"
+    sudo apt-get install unzip -y < "/dev/null"
+    wget -O $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY_ARCHIVE https://github.com/grafana/loki/releases/download/$NG_PROMTAIL_RELEASE/$NG_PROMTAIL_BINARY_ARCHIVE
+    unzip $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY_ARCHIVE
+    cp $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY /usr/local/bin/
+    wget -O $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME https://raw.githubusercontent.com/grafana/loki/main/clients/cmd/promtail/$NG_PROMTAIL_CONFIG_NAME
+    sed -i 's/localhost:3100/$NG_PROMTAIL_LOKI_SERVER_ADDRESS/g' $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
+    sed -i 's|*log|caddy/*log|g' $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
+    echo "Promtail installed and configured"
 fi
 
-#Configuring in case of matching config file
-if grep localhost:3100 $HOME/promtail-local-config.yaml
-then
-echo "Configuring Loki server"
-sudo sed -i 's/localhost:3100/195.201.98.42:3100/g' promtail-local-config.yaml
+# Configuring in case of matching config file
+if grep localhost:3100 $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME; then
+    echo "Configuring Loki server"
+    sed -i 's/localhost:3100/$NG_PROMTAIL_LOKI_SERVER_ADDRESS/g' $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
 else
-echo "Config file hasn't beed found and will be installed and configured"
-sudo wget https://raw.githubusercontent.com/grafana/loki/main/clients/cmd/promtail/promtail-local-config.yaml
-sudo sed -i 's/localhost:3100/195.201.98.42:3100/g' promtail-local-config.yaml
+    echo "Config file hasn't beed found and will be installed and configured"
+    wget -O $NG_PROMTAIL_CONFIG_NAME https://raw.githubusercontent.com/grafana/loki/main/clients/cmd/promtail/$NG_PROMTAIL_CONFIG_NAME
+    sed -i 's/localhost:3100/$NG_PROMTAIL_LOKI_SERVER_ADDRESS/g' $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
 fi
 
-if grep log/*log $HOME/promtail-local-config.yaml
-then
-echo "Configuring logging path"
-sudo sed -i 's|*log|caddy/*log|g' promtail-local-config.yaml
-echo "Done"
+if grep log/*log $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME; then
+    echo "Configuring logging path"
+    sed -i 's|*log|caddy/*log|g' $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
+    echo "Done"
 else
-echo "Logging path already been modified"
+    echo "Logging path already been modified"
 fi
 
-#Checking Promtail exec file"
-if -f $HOME/promtail-linux-amd64
-then
-echo "Promtail exec file installed in home directory"
+# Checking Promtail exec file"
+if [ -f $(which $NG_PROMTAIL_BINARY) ]; then
+    echo "Promtail installed in: $(which $NG_PROMTAIL_BINARY)"
 else
-echo "Promtail exec file hasn't been found and will be installed"
-echo "Zip app will be installed to help Promtail installation"
-sudo apt-get install unzip -y
-cd $HOME
-sudo wget https://github.com/grafana/loki/releases/download/v3.0.0/promtail-linux-amd64.zip
-sudo unzip ./promtail-linux-amd64.zip
-echo "Promtail exec file is installed in:"
-echo $HOME
+    echo "Promtail exec file hasn't been found and will be installed"
+    echo "Unzip package will be installed to help Promtail installation"
+    sudo apt-get install unzip -y < "/dev/null"
+    cd $NG_PROMTAIL_DIR
+    wget -O $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY_ARCHIVE https://github.com/grafana/loki/releases/download/$NG_PROMTAIL_RELEASE/$NG_PROMTAIL_BINARY_ARCHIVE
+    unzip $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY_ARCHIVE
+    cp $NG_PROMTAIL_DIR/$NG_PROMTAIL_BINARY /usr/local/bin/$NG_PROMTAIL_BINARY
+    echo "Promtail exec file is installed in:" $(which $NG_PROMTAIL_BINARY)
+fi
 
-#Service configuration
-touch /etc/systemd/system/promtail.service
+# Service configuration
+# touch /etc/systemd/system/promtail.service
 sudo cat > /etc/systemd/system/promtail.service << EOF
 [Unit]
 Description=Promtail service
@@ -63,7 +76,7 @@ After=network.target
 Type=simple
 User=root
 Group=root
-ExecStart=$HOME/promtail-linux-amd64 -config.file $HOME/promtail-local-config.yaml
+ExecStart=$(which $NG_PROMTAIL_BINARY) -config.file $NG_PROMTAIL_DIR/$NG_PROMTAIL_CONFIG_NAME
 # Give a reasonable amount of time for promtail to start up/shut down
 TimeoutSec = 60
 Restart = on-failure
@@ -75,4 +88,5 @@ EOF
 sudo systemctl daemon-restart
 sudo systemctl enable promtail.service
 sudo systemctl start promtail.service
+sudo journalctl -u promtail -f -o cat
 echo "Things must work well. Please report any misconfigurations in discord"
